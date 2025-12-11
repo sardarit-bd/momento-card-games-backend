d8cde035834b49afb2249bba7f6dd7f6458fafd7
shahadat <funkyd3v@gmail.com>
2025-12-11 14:49:24 +0600

    role middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'roles' => App\Http\Middleware\RoleCheck::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
